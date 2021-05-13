5580b79d3cfba0352f18a126274f9232942ef303
Ninja1672 <powellhunter1997@gmail.com>
2021-05-13 10:59:54 -0600

    Update player.py
    Player class done

--- hilo_template/hilo/game/player.py
+++ hilo_template/hilo/game/player.py
@@ -1,6 +1,30 @@
 class player:
+    #do guess only allows h or l and repeats if invalid answer given
     def do_guess(self):
-        guess = input("Higher or Lower? [h/l]: ")
-        return guess
-    def is_hilo(self):
-        pass
+        while True:
+            guess = input("Higher or Lower? [h/l]: ")
+            if guess == 'h' or guess == 'l':
+                return guess
+                
+            else:
+                print("Please only give H or L for higher or lower.")
+    #is hilo will call guess and compare the 2 cards given from dealer. Will return point totals
+    #specified by the requirements
+    def is_hilo(self, old_card, new_card):
+        guess = self.do_guess()
+        if guess == 'h':
+            if new_card > old_card:
+                return 100
+            else:
+                return -75
+        elif guess == 'l':
+            if new_card < old_card:
+                return 100
+            else: 
+                return -75
+
+#This is to test if things are working right uncomment if you want to make sure player class works
+#x = player()
+#print(x.is_hilo(10,11))
+#print("done")
+        
